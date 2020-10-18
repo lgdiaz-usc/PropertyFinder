5cd7090d3111706b73cc3cc1058751b1da1e6a3b
lgdiaz-usc <lgdiaz@email.sc.edu>
2020-10-18 19:10:21 -0400

    Added a stub for the class Property and renamed System to PSystem

--- PSystem.java
+++ PSystem.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 /**
  * Contains the methods and data used by the driver to interact with user accounts and property listings.
  */
-public class System {
+public class PSystem {
     protected ArrayList<Property> properties;
     protected ArrayList<User> users;
     protected ArrayList<PropertyManager> propertyManagers;
@@ -14,15 +14,15 @@ public class System {
     /**
      * Default constructor for System.
      */
-    public System(){
+    public PSystem(){
 
     }
 
     /**
      * Parameterized constructor for System
-     * @param properties -> A preexisting list of Property's
-     * @param users -> A preexisting list of User's
-     * @param propertyManagers -> A preexisting list of PropertyManager's
+     * @param properties A preexisting list of Property's
+     * @param users A preexisting list of User's
+     * @param propertyManagers A preexisting list of PropertyManager's
      */
     public System(ArrayList<Property> properties, ArrayList<User> users,
                   ArrayList<PropertyManager> propertyManagers){
@@ -31,14 +31,14 @@ public class System {
 
     /**
      * Creates a new User object and adds it to users
-     * @param username -> The username for the User
-     * @param password -> The password for the User
-     * @param name -> The name of the User
-     * @param datOfBirth -> The date of birth of the User
-     * @param homeAddress -> The home address of the User
-     * @param email -> The email address of the User
-     * @param phoneNumber -> The phone number of the User
-     * @param StudentID -> The student ID of the User
+     * @param username The username for the User
+     * @param password The password for the User
+     * @param name The name of the User
+     * @param datOfBirth The date of birth of the User
+     * @param homeAddress The home address of the User
+     * @param email The email address of the User
+     * @param phoneNumber The phone number of the User
+     * @param StudentID The student ID of the User
      */
     public void createUserAccount(String username, String password, String name,
                                   String datOfBirth, String homeAddress, String email,
@@ -48,13 +48,13 @@ public class System {
 
     /**
      * Creates a new PropertyManager object and adds it to propertyManagers
-     * @param username -> The username for the PropertyManager
-     * @param password -> The password for the PropertyManager
-     * @param name -> The name of the PropertyManager
-     * @param datOfBirth -> The date of birth of the PropertyManager
-     * @param homeAddress -> The home address of the PropertyManager
-     * @param email -> The email address of the PropertyManager
-     * @param phoneNumber -> The phone number of the PropertyManager
+     * @param username The username for the PropertyManager
+     * @param password The password for the PropertyManager
+     * @param name The name of the PropertyManager
+     * @param datOfBirth The date of birth of the PropertyManager
+     * @param homeAddress The home address of the PropertyManager
+     * @param email The email address of the PropertyManager
+     * @param phoneNumber The phone number of the PropertyManager
      */
     public void createManagerAccount(String username, String password, String name,
                                   String datOfBirth, String homeAddress, String email,
@@ -64,8 +64,8 @@ public class System {
 
     /**
      * Allow the user to log in to their Account
-     * @param username -> The username of the Account
-     * @param Password -> The password of the Account
+     * @param username The username of the Account
+     * @param Password The password of the Account
      */
     public void login(String username, String Password){
 
@@ -80,7 +80,7 @@ public class System {
 
     /**
      * Returns the Terms of Service
-     * @return -> The Terms of Service
+     * @return The Terms of Service
      */
     public String getTOS(){
 
@@ -88,8 +88,8 @@ public class System {
 
     /**
      * Searches properties for Property's the match query
-     * @param query -> The terms to be searched for
-     * @return -> A list of all Property's that matched the query
+     * @param query The terms to be searched for
+     * @return A list of all Property's that matched the query
      */
     public ArrayList<Property> searchProperty(String query){
 
@@ -97,8 +97,8 @@ public class System {
 
     /**
      * Searches propertyManagers for PropertyManager's the match query
-     * @param query -> The terms to be searched for
-     * @return -> A list of all PropertyManager's that matched the query
+     * @param query The terms to be searched for
+     * @return A list of all PropertyManager's that matched the query
      */
     public ArrayList<PropertyManager> searchManager(String query){
 
@@ -106,8 +106,8 @@ public class System {
 
     /**
      * Searches users for User's the match query
-     * @param query -> The terms to be searched for
-     * @return -> A list of all User's that matched the query
+     * @param query The terms to be searched for
+     * @return A list of all User's that matched the query
      */
     public ArrayList<User> searchUser(String query){
 
@@ -115,9 +115,9 @@ public class System {
 
     /**
      * Sorts a list of Property's by title alphabetically
-     * @param properties -> The list of Property's to be sorted
-     * @param descending -> Whether or not to sort by descending
-     * @return -> The sorted list of Property's
+     * @param properties The list of Property's to be sorted
+     * @param descending Whether or not to sort by descending
+     * @return The sorted list of Property's
      */
     public ArrayList<Property> sortPropertyByTitle(ArrayList<Property> properties, boolean descending){
 
@@ -125,9 +125,9 @@ public class System {
 
     /**
      * Sorts a list of Property's by price
-     * @param properties -> The list of Property's to be sorted
-     * @param descending -> Whether or not to sort by descending
-     * @return -> The sorted list of Property's
+     * @param properties The list of Property's to be sorted
+     * @param descending Whether or not to sort by descending
+     * @return The sorted list of Property's
      */
     public ArrayList<Property> sortPropertyByPrice(ArrayList<Property> properties, boolean descending){
 
@@ -135,9 +135,9 @@ public class System {
 
     /**
      * Sorts a list of Property's by average rating
-     * @param properties -> The list of Property's to be sorted
-     * @param descending -> Whether or not to sort by descending
-     * @return -> The sorted list of Property's
+     * @param properties The list of Property's to be sorted
+     * @param descending Whether or not to sort by descending
+     * @return The sorted list of Property's
      */
     public ArrayList<Property> sortPropertyByRating(ArrayList<Property> properties,
                                                     boolean descending){
@@ -146,9 +146,9 @@ public class System {
 
     /**
      * Sorts a list of PropertyManger's by name alphabetically
-     * @param managers -> The list of Property's to be sorted
-     * @param descending -> Whether or not to sort by descending
-     * @return -> The sorted list of PropertyManager's
+     * @param managers The list of Property's to be sorted
+     * @param descending Whether or not to sort by descending
+     * @return The sorted list of PropertyManager's
      */
     public ArrayList<PropertyManager> sortManagerByName(ArrayList<PropertyManager> managers,
                                                         boolean descending){
@@ -157,9 +157,9 @@ public class System {
 
     /**
      * Sorts a list of PropertyManger's by average rating
-     * @param managers -> The list of Property's to be sorted
-     * @param descending -> Whether or not to sort by descending
-     * @return -> The sorted list of PropertyManager's
+     * @param managers The list of Property's to be sorted
+     * @param descending Whether or not to sort by descending
+     * @return The sorted list of PropertyManager's
      */
     public ArrayList<PropertyManager> sortManagerByRating(ArrayList<PropertyManager> managers,
                                                           boolean descending){
@@ -168,9 +168,9 @@ public class System {
 
     /**
      * Sorts a list of User's by name alphabetically
-     * @param users -> The list of Property's to be sorted
-     * @param descending -> Whether or not to sort by descending
-     * @return -> The sorted list of User's
+     * @param users The list of Property's to be sorted
+     * @param descending Whether or not to sort by descending
+     * @return The sorted list of User's
      */
     public ArrayList<User> sortUserByName(ArrayList<User> users, boolean descending){
 
@@ -178,9 +178,9 @@ public class System {
 
     /**
      * Sorts a list of User's by average rating
-     * @param users -> The list of Property's to be sorted
-     * @param descending -> Whether or not to sort by descending
-     * @return -> The sorted list of User's
+     * @param users The list of Property's to be sorted
+     * @param descending Whether or not to sort by descending
+     * @return The sorted list of User's
      */
     public ArrayList<User> sortUserByRating(ArrayList<User> users, boolean descending){
 
@@ -188,8 +188,8 @@ public class System {
 
     /**
      * Reverses the order of a list.
-     * @param list -> The lis to be reversed
-     * @return -> The reversed list
+     * @param list The lis to be reversed
+     * @return The reversed list
      */
     private ArrayList descendSort(ArrayList list){
 
@@ -197,8 +197,8 @@ public class System {
 
     /**
      * Creates a Message object and sends it to the specified PropertyManager object
-     * @param managerName -> The username of the PropertyManager being contacted
-     * @param message -> The contents of the message
+     * @param managerName The username of the PropertyManager being contacted
+     * @param message The contents of the message
      */
     public void contactManager(String managerName, String message){
 
@@ -206,8 +206,8 @@ public class System {
 
     /**
      * Creates a Message object and sends it to the specified User object
-     * @param userName -> The username of the User being contacted
-     * @param message -> The contents of the message
+     * @param userName The username of the User being contacted
+     * @param message The contents of the message
      */
     public void contactUser(String userName, String message) {
 
@@ -216,7 +216,7 @@ public class System {
     /**
      * Returns the String interpretations for all Message's in the Account corresponding to
      * currentAccount
-     * @return -> The /string interpretation of the current user's Message's
+     * @return The /string interpretation of the current user's Message's
      */
     public String getMessages(){
 
@@ -224,8 +224,8 @@ public class System {
 
     /**
      * Adds a User to a Property
-     * @param propertyTitle -> The title of the Property being edited
-     * @param renterName -> The username of the User being added to the Property
+     * @param propertyTitle The title of the Property being edited
+     * @param renterName The username of the User being added to the Property
      */
     public void addRenter(String propertyTitle, String renterName){
 
@@ -233,8 +233,8 @@ public class System {
 
     /**
      * Removes a User to a Property
-     * @param propertyTitle -> The title of the Property being edited
-     * @param renterName -> The username of the User being removed to the Property
+     * @param propertyTitle The title of the Property being edited
+     * @param renterName The username of the User being removed to the Property
      */
     public void removeRenter(String propertyTitle, String renterName){
 
@@ -242,9 +242,9 @@ public class System {
 
     /**
      * Adds a User to a Unit
-     * @param propertyTitle -> The title of the Property being edited
-     * @param addressModifier -> The addressModifier that identifies the Unit being edited
-     * @param renterName -> The username of the USer being added
+     * @param propertyTitle The title of the Property being edited
+     * @param addressModifier The addressModifier that identifies the Unit being edited
+     * @param renterName The username of the USer being added
      */
     public void addUnitRenter(String propertyTitle, String addressModifier, String renterName){
 
@@ -252,9 +252,9 @@ public class System {
 
     /**
      * Removes a User to a Unit
-     * @param propertyTitle -> The title of the Property being edited
-     * @param addressModifier -> The addressModifier that identifies the Unit being edited
-     * @param renterName -> The username of the USer being removed
+     * @param propertyTitle The title of the Property being edited
+     * @param addressModifier The addressModifier that identifies the Unit being edited
+     * @param renterName The username of the USer being removed
      */
     public void removeUnitRenter(String propertyTitle, String addressModifier, String renterName){
 
@@ -263,10 +263,10 @@ public class System {
     /**
      * Creates a Property object and adds it to properties
      * @param title ->The title of the Property
-     * @param description -> The description of the Property
-     * @param address -> The address of the Property
-     * @param capacity -> The maximum capacity of the Property
-     * @param baseRent -> The base amount of rent for the Property
+     * @param description The description of the Property
+     * @param address The address of the Property
+     * @param capacity The maximum capacity of the Property
+     * @param baseRent The base amount of rent for the Property
      */
     public void addProperty(String title, String description, String address, int capacity,
                             double baseRent){
@@ -275,9 +275,9 @@ public class System {
 
     /**
      * Adds a Unit to a Property
-     * @param propertyName -> The title of the Property to be edited
-     * @param addressModifier -> The addressModifier of the Unit
-     * @param capacity -> The maximum capacity of the Unit
+     * @param propertyName The title of the Property to be edited
+     * @param addressModifier The addressModifier of the Unit
+     * @param capacity The maximum capacity of the Unit
      */
     public void addUnit(String propertyName, String addressModifier, int capacity){
 
@@ -285,7 +285,7 @@ public class System {
 
     /**
      * Removes a Property from properties
-     * @param propertyName -> The title of the Property to be removed
+     * @param propertyName The title of the Property to be removed
      */
     public void removeProperty(String propertyName) {
 
@@ -293,8 +293,8 @@ public class System {
 
     /**
      * Removes a Unit from a Property
-     * @param propertyName -> The title of Property to be edited
-     * @param addressModifier -> The addressModifier of the Unit to be removed
+     * @param propertyName The title of Property to be edited
+     * @param addressModifier The addressModifier of the Unit to be removed
      */
     public void removeUnit(String propertyName, String addressModifier){
 
@@ -302,9 +302,9 @@ public class System {
 
     /**
      * Edits a Property based on EditType
-     * @param propertyName -> The title of the Property to be edited
-     * @param change -> The replacement String
-     * @param type -> Which String is to be replaced
+     * @param propertyName The title of the Property to be edited
+     * @param change The replacement String
+     * @param type Which String is to be replaced
      */
     public void editProperty(String propertyName, String change, EditType type){
 
@@ -312,10 +312,10 @@ public class System {
 
     /**
      * Edits a Property based on EditType
-     * @param propertyName -> The title of the Property to be edited
-     * @param addressModifier -> The addressModifier of the Unit to be edited
-     * @param change -> The replacement String
-     * @param type -> Which String is to be replaced
+     * @param propertyName The title of the Property to be edited
+     * @param addressModifier The addressModifier of the Unit to be edited
+     * @param change The replacement String
+     * @param type Which String is to be replaced
      */
     public void editUnit(String propertyName, String addressModifier, String change, EditType type){
 
@@ -323,9 +323,9 @@ public class System {
 
     /**
      * Adds an additional fee to a Property
-     * @param propertyName -> The title of the Property to be edited
-     * @param name -> The name of the fee
-     * @param fee -> The dollar amount of the fee
+     * @param propertyName The title of the Property to be edited
+     * @param name The name of the fee
+     * @param fee The dollar amount of the fee
      */
     public void addFee(String propertyName, String name, double fee){
 
@@ -333,8 +333,8 @@ public class System {
 
     /**
      * Removes an additional fee from a Property
-     * @param propertyName -> The title of the Property to be edited
-     * @param name -> The name of the fee to be removed
+     * @param propertyName The title of the Property to be edited
+     * @param name The name of the fee to be removed
      */
     public void removeFee(String propertyName, String name){
 
@@ -342,11 +342,11 @@ public class System {
 
     /**
      * Adds a Review to an object based on ReviewType
-     * @param rating -> Rating of the Review (out of 10)
-     * @param title -> The title of the Review
-     * @param description -> The description of the Review
-     * @param reviewSubject -> the username/title/addressModifier of the object being reviewed
-     * @param Type -> The type of object being reviewed
+     * @param rating Rating of the Review (out of 10)
+     * @param title The title of the Review
+     * @param description The description of the Review
+     * @param reviewSubject the username/title/addressModifier of the object being reviewed
+     * @param Type The type of object being reviewed
      */
     public void addReview(int rating, String title, String description, String reviewSubject,
                           ReviewType Type){
@@ -355,7 +355,7 @@ public class System {
 
     /**
      * Returns the current Account if it is a User
-     * @return -> The current Account
+     * @return The current Account
      */
     public User getUser() {
 
@@ -363,7 +363,7 @@ public class System {
 
     /**
      * Returns the current Account if it is a PropertyManager
-     * @return -> The current Account
+     * @return The current Account
      */
     public PropertyManager getManager(){
 
@@ -387,7 +387,7 @@ public class System {
 
     /**
      * Returns all of the data in users, propertyManagers, and properties in the JSON format
-     * @return -> The Json interpretation of System's data
+     * @return The Json interpretation of System's data
      */
     public String toJSON(){
 
---
+++ Property.java
@@ -0,0 +1,276 @@
+package PropertyFinder;
+
+import java.util.ArrayList;
+
+/**
+ * Contains data and methods related to Property listings
+ */
+public class Property {
+    private String manager;
+    private String title;
+    private String description;
+    private String address;
+    private ArrayList<String> renters;
+    private int capacity;
+    private ArrayList<Unit> units;
+    private ArrayList<Review> reviews;
+    private double baseRent;
+    private ArrayList<String> extraFees;
+
+    /**
+     * Parameterized constructor for Property
+     * @param manager The username of the PropertyManager that owns this Property
+     * @param title The title of this Property listing
+     * @param description The description of the Property listing
+     * @param address The address of this Property
+     * @param capacity The maximum capacity of this Property
+     * @param baseRent The base amount of rent for this Property
+     */
+    public Property(String manager, String title, String description, String address,
+                    int capacity, double baseRent) {
+
+    }
+
+    /**
+     * Creates and adds a Review object to reviews
+     * @param rating The rating of the Review (out of 10)
+     * @param title The title of the Review
+     * @param description The description for the Review
+     * @param author The username of the Account that made the Review
+     */
+    public void addReview(int rating, String title, String description, Account author){
+
+    }
+
+    /**
+     * Creates and adds a Review object to the reviews array of a specified Unit
+     * @param rating The rating of the Review (out of 10)
+     * @param title The title of the Review
+     * @param description The description for the Review
+     * @param author The username of the Account that made the Review
+     * @param addressModifier The addressModifier corresponding to the specified Unit
+     */
+    public void addUnitReview(int rating, String title, String description, Account author,
+                              String addressModifier){
+
+    }
+
+    /**
+     * Returns the average rating for all Review objects is reviews
+     * @return The average rating
+     */
+    public int getAverageRating(){
+
+    }
+
+    /**
+     * Returns the average rating for all Review objects in the reviews array of a specified Unit
+     * @param addressModifier The addressModifier which identifies the requested Unit
+     * @return The average rating
+     */
+    public int getUnitAverageRating(String addressModifier){
+
+    }
+
+    /**
+     * Returns whether or not the Property is available for rent
+     * @return
+     */
+    public boolean isAvailable() {
+
+    }
+
+    /**
+     * Returns whether the current Account is the PropertyManager which owns this Property
+     * @param currentAccount The username of the current Account
+     * @return Whether or not the current Account is the owner of this Property
+     */
+    public boolean isManager(String currentAccount) {
+
+    }
+
+    /**
+     * Returns whether or not the current Account is a renter of this Property
+     * @param renterName The username of the current Account
+     * @return Whether or not the current Account is a renter of this Property
+     */
+    public boolean isRenter(String renterName) {
+
+    }
+
+    /**
+     * Adds a renter to renters
+     * @param renter The username of the User being added
+     * @param currentAccount The current Account
+     */
+    public void addRenter(String renter, String currentAccount) {
+
+    }
+
+    /**
+     * Adds a renter to the renters array of a requested Unit
+     * @param renter The username of the User being added
+     * @param addressModifier The addressModifier of the Unit being edited
+     * @param currentAccount The current Account
+     */
+    public void addUnitRenter(String renter, String addressModifier, String currentAccount) {
+
+    }
+
+    /**
+     * Removes a renter from renters
+     * @param renter The username of the User being removed
+     * @param currentAccount The current Account
+     */
+    public void removeRenter(String renter, String currentAccount) {
+
+    }
+
+    /**
+     * removes a renter from the renters array of a requested Unit
+     * @param renter The username of the User being removed
+     * @param addressModifier The addressModifier of the Unit being edited
+     * @param currentAccount The current Account
+     */
+    public void removeUnitRenter(String renter, String addressModifier, String currentAccount) {
+
+    }
+
+    /**
+     * Creates and adds a Unit to units 
+     * @param addressModifier The addressModifier of the Unit
+     * @param capacity The maximum capacity of the Unit
+     * @param currentAccount The current Account
+     */
+    public void addUnit(String addressModifier, int capacity, String currentAccount) {
+
+    }
+
+    /**
+     * Removes a Unit from units
+     * @param addressModifier The addressModifier of the Unit being removed
+     * @param currentAccount The current Account
+     */
+    public void removeUnit(String addressModifier, String currentAccount) {
+
+    }
+
+    /**
+     * Adds an extra fee to extraFees
+     * @param currentAccount The current Account
+     * @param name The name of the fee (water, electricity, internet, etc.)
+     * @param fee The dollar amount of the fee
+     */
+    public void addFee(String currentAccount, String name, double fee) {
+
+    }
+
+    /**
+     * Removes an extra fee from extraFees
+     * @param currentAccount The current Account
+     * @param name The name of the fee
+     */
+    public void removeFee(String currentAccount, String name) {
+        
+    }
+
+    /**
+     * Updates title
+     * @param currentAccount The current Account
+     * @param title The new title
+     */
+    public void updateTitle(String currentAccount, String title) {
+
+    }
+
+    /**
+     * Updates address
+     * @param currentAccount The current Account
+     * @param address The new address
+     */
+    public void updateAddress(String currentAccount, String address) {
+
+    }
+
+    /**
+     * Updates description
+     * @param currentAccount The current Account
+     * @param description The new description
+     */
+    public void updateDescription(String currentAccount, String description) {
+
+    }
+
+    /**
+     * Updates the maximum capacity
+     * @param currentAccount The current Account
+     * @param capacity The new capacity
+     */
+    public void updateCapacity(String currentAccount, int capacity) {
+
+    }
+
+    /**
+     * Updates the base rent
+     * @param currentAccount The current Account
+     * @param baseRent The new base rent
+     */
+    public void updateRent(String currentAccount, double baseRent) {
+
+    }
+
+    /**
+     * Updates the maximum capacity of a requested Unit
+     * @param currentAccount The current Account
+     * @param addressModifier The addressModifier of the Unit being requested
+     * @param capacity The new maximum capacity
+     */
+    public void updateUnitCapacity(String currentAccount, String addressModifier, int capacity){
+
+    }
+
+    /**
+     * Updates the addressModifier of a requested Unit
+     * @param currentAccount The current Account
+     * @param addressModifier The addressModifier of the Unit being requested
+     * @param newModifier The new addressModifier
+     */
+    public void updateUnitAddressModifier(String currentAccount, String addressModifier,
+                                          String newModifier) {
+
+    }
+
+    /**
+     * Updates a renter's username
+     * @param currentAccount The username of the renter being updated
+     * @param newRenterName The renter's new username
+     */
+    public void updateRenterUsername(String currentAccount, String newRenterName) {
+
+    }
+
+    /**
+     * Updates the username of the PropertyManager that owns this Property
+     * @param currentAccount The old username of the owner
+     * @param newManager The owner's new username
+     */
+    public void updateManagerUsername(String currentAccount, String newManager) {
+
+    }
+
+    /**
+     * Returns a description of the Property listing as a String
+     * @return -> The description of the Property listing
+     */
+    public String toString(){
+
+    }
+
+    /**
+     * Return the contents of this Property as a String in the JSON format
+     * @return -> The contents of this Property
+     */
+    public String toJSON(){
+
+    }
+}
