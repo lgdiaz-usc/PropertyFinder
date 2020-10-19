6b9b4a8fda2737d9ac5647541637878be33af5eb
sincere99 <70166640+sincere99@users.noreply.github.com>
2020-10-19 01:11:42 -0400

    Added a stub for the class Message

---
+++ Message.java
@@ -0,0 +1,33 @@
+package PropertyFinder;
+
+/**
+ * Contains data and methods related to User's messages
+ */
+public class Message {
+	private String author;
+	private String description;
+	
+	/**
+	 * Parameterized constructor for Message
+	 * @param author -> The author of the message
+	 * @param message -> The message of the author
+	 */
+	public Message(String author, String message) {
+		
+	}
+	
+	/**
+	 * Returns a String interpretation of the Message
+	 */
+	public String toString() {
+		
+	}
+	
+	/**
+	 * Returns the contents of Message in JSON format
+	 * @return The JSON interpretation of the Message contents
+	 */
+	public String toJSON() {
+		
+	}
+}
