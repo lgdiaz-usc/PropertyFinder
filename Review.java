c1d12142cece43a38ead559da0aa152da219d4df
sincere99 <70166640+sincere99@users.noreply.github.com>
2020-10-19 01:47:32 -0400

    Added a stub for the class Review

---
+++ Review.java
@@ -0,0 +1,44 @@
+package PropertyFinder;
+
+/**
+ * Contains data and methods related to reviews
+ */
+public class Review {
+	private int rating;
+	private String title;
+	private String description;
+	private Account author;
+	
+	/**
+	 * The parameterized constructor for Review
+	 * @param rating -> The rating of the review
+	 * @param title -> The title of the review
+	 * @param description -> The description of the review
+	 */
+	public  Review(int rating, String title, String description) {
+		
+	}
+	
+	/**
+	 * Returns the rating of the review
+	 * @return The rating of the review
+	 */
+	public int getRating() {
+		
+	}
+	
+	/**
+	 * Returns a String interpretation of the User
+	 */
+	public String toString() {
+		
+	}
+	
+	/**
+	 * Returns the contents of Review in JSON format
+	 * @return The JSON interpretation of the Review contents
+	 */
+	public String toJSON() {
+		
+	}
+}
