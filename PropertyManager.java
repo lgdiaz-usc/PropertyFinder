0823e0d82a485c5604fd98cff59543e58f315614
lgdiaz-usc <lgdiaz@email.sc.edu>
2020-10-18 21:04:38 -0400

    Added a stub for the class PropertyManager

---
+++ PropertyManager.java
@@ -0,0 +1,21 @@
+package PropertyFinder;
+
+/**
+ * Contains methods and data for property manager accounts
+ */
+public class PropertyManager extends Account{
+    /**
+     * Parameterized constructor fo PropertyManager
+     * @param username The username of the PropertyManager
+     * @param password The passwrod of the PropertyManager
+     * @param name The name of the PropertyManager
+     * @param dateOfBirth The date of birth of the PropertyManager
+     * @param homeAddress The home address of the PropertyManager
+     * @param email The email address of th ePropertyManager
+     * @param phoneNumber The phone number of the PropertyManager
+     */
+    public PropertyManager(String username, String password, String name, String dateOfBirth,
+                           String homeAddress, String email, String phoneNumber){
+
+    }
+}
