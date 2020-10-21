3510dbcbc511e594382eac632a42fce64614451d
lgdiaz-usc <lgdiaz@email.sc.edu>
2020-10-21 13:44:03 -0400

    Created a stub for the class DataWriter

---
+++ DataWriter.java
@@ -0,0 +1,18 @@
+package PropertyFinder;
+
+import org.json.simple.JSONObject;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * Collects the JSON data of a PSystem object and writes it to a file
+ */
+public class DataWriter {
+    /**
+     * Writes the JSON data of a PSystem object to a file
+     * @param system The PSystem object whose JSON data is being written
+     */
+    public static void write(PSystem system){
+
+    }
+}
