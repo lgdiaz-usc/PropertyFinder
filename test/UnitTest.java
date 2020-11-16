f05207790e5714fbe7185595ac5ae7cc4ff677b1
sincere99 <70166640+sincere99@users.noreply.github.com>
2020-11-15 23:53:21 -0500

    Added test for negative ratings

--- test/UnitTest.java
+++ test/UnitTest.java
@@ -13,20 +13,29 @@ class UnitTest {
     void setUp() {
         unit = new Unit("#4a", 4);
     }
-
+    
     @Test
-    void testAverageRatingOF0(){
+    void testAverageRatingOfZero() {
         int average = unit.getAverageRating();
         Assertions.assertTrue(average == 0);
     }
 
     @Test
-    void testAverageRatingOf5(){
+    void testAverageRatingOfPositiveNumbers() {
         unit.addReview(10, "is good", "very good", "manguy");
         unit.addReview(2, "not so good", "is bad", "guyman");
         unit.addReview(3, "slightly ok", "not good", "ganmuy");
         int average = unit.getAverageRating();
         Assertions.assertTrue(average == 5);
+    }
+    
+    @Test
+    void testAverageRatingOfNegativeNumbers() {
+    	unit.addReview(-6, "is good", "very good", "manguy");
+        unit.addReview(-7, "not so good", "is bad", "guyman");
+        unit.addReview(-8, "slightly ok", "not good", "ganmuy");
+        int average = unit.getAverageRating();
+        Assertions.assertTrue(average == -7);
     }
 
     @Test
