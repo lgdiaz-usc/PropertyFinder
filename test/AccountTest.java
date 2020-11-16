8f1221f2249843bdf8f054364aae55cb8f82cd73
lgdiaz-usc <lgdiaz@email.sc.edu>
2020-11-15 21:14:01 -0500

    Added test folder and empty AccountTest

---
+++ test/AccountTest.java
@@ -0,0 +1,31 @@
+package PropertyFinder.test;
+
+import org.junit.jupiter.api.*;
+
+class AccountTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void authenticate() {
+    }
+
+
+    @Test
+    void testToString() {
+    }
+
+    @Test
+    void addMessage() {
+    }
+
+    @Test
+    void toJSON() {
+    }
+}
