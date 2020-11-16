4ecf9cf06541a03af3156eebe94b6268a0aa0091
lgdiaz-usc <lgdiaz@email.sc.edu>
2020-11-15 22:52:14 -0500

    Added test cases for Property

---
+++ test/PropertyTest.java
@@ -0,0 +1,72 @@
+package PropertyFinder.test;
+
+import PropertyFinder.Property;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PropertyTest {
+    private Property property;
+
+    @BeforeEach
+    void setUp(){
+        property = new Property("bill", "bill\'s house", "its bill\'s house", "123 street st.", 2, 5);
+    }
+
+    @Test
+    void testAverageRatingOF0(){
+        int average = property.getAverageRating();
+        Assertions.assertTrue(average == 0);
+    }
+
+    @Test
+    void testAverageRatingOf5(){
+        property.addReview(10, "is good", "very good", "manguy");
+        property.addReview(2, "not so good", "is bad", "guyman");
+        property.addReview(3, "slightly ok", "not good", "ganmuy");
+        int average = property.getAverageRating();
+        Assertions.assertTrue(average == 5);
+    }
+
+    @Test
+    void isAvailable() {
+        boolean hasSpace = property.isAvailable();
+        assertTrue(hasSpace);
+    }
+
+    @Test
+    void isNotAvailable() {
+        property.addRenter("renter 1", "bill");
+        property.addRenter("renter 2", "bill");
+        boolean hasSpace = property.isAvailable();
+        assertFalse(hasSpace);
+    }
+
+    @Test
+    void isManager() {
+        boolean manager = property.isManager("bill");
+        assertTrue(manager);
+    }
+
+    @Test
+    void isNotManager() {
+        boolean manager = property.isManager("not bill");
+        assertFalse(manager);
+    }
+
+    @Test
+    void isRenter() {
+        property.addRenter("bingus", "bill");
+        boolean renter = property.isRenter("bingus");
+        assertTrue(renter);
+    }
+
+    @Test
+    void isNotRenter() {
+        property.addRenter("bingus", "bill");
+        boolean renter = property.isRenter("not bingus");
+        assertFalse(renter);
+    }
+}
